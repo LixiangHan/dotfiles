dae355f79791901da8eba6e143bfca279a0dd33b
lixianghan <lixianghan98@gmail.com>
2025-09-21 16:03:05 +0800

    neovim + lazyvim

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-echo "Install vim configuration..."
+echo "Install NeoVim..."
+
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.appimage
+
+chmod u+x nvim-linux-x86_64.appimage
+
+mv nvim-linux-x86_64.appimage ~/.local/bin/nvim
+
+export PATH="$HOME/.local/bin:$PATH"
+
+rm nvim-linux-x86_64.appimage
+
+echo "Setup LazyVim..."
 
 cd ~/dotfiles
 
-if [ -f "$HOME/.vimrc" ]; then
-	mv ~/.vimrc ~/.vimrc.`date +%Y%m%d`
+if [ -f "$HOME/.config/nvim" ]; then
+  mv ~/.config/nvim ~/.config/nvim.$(date +%Y%m%d)
 fi
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-
-git submodule init
-git submodule update
-
-vi +BundleInstall! +qall
-
+ln -s ~/dotfiles/nvim ~/.config/nvim
